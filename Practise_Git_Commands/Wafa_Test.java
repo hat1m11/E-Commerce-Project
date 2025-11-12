6a567b6e6b347753f7cc36cec15a90e426e271e6
Wafa676 <230018613@aston.ac.uk>
2025-11-12 18:52:52 +0000

    Added Wafa_Test.java to test Rohans Modulus Function

---
+++ Practise_Git_Commands/Wafa_Test.java
@@ -0,0 +1,23 @@
+/*******************************
+ * Tester: Wafa Ali 
+ * University ID: 230018613
+ * Function: This function tests the Modulus() function implemented by Rohan.
+ */
+
+public class Wafa_Test {
+    public static void main(String[] args) 
+    Math math = new Math();
+        
+    //Test: 20 % 3 = 2
+    int result = math.Modulus(20, 3);
+    
+    if (result == 2) {
+        System.out.println(x:"Test Passed: Modulus(20, 3) returned 2");
+    } else {
+        System.out.println(x: "Test Failed: Modulus(20, 3) returned " + result);
+        
+
+    }
+
+    
+}
