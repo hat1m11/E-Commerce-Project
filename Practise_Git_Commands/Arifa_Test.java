a5638ac53a60b21898a1ac363b4f7691f9ec7413
arifa212 <240189703@aston.ac.uk>
2025-11-12 21:31:33 +0000

    Arifas test for Yusufs function

---
+++ Practise_Git_Commands/Arifa_Test.java
@@ -0,0 +1,13 @@
+
+public class Arifa_Test{
+    public static void main(String[] args){
+        Math math = new Math();
+        int result = math.subtract(10,3);
+
+        if(result==7){
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+        }
+    }
--- Practise_Git_Commands/Math.java
+++ Practise_Git_Commands/Math.java
@@ -73,3 +73,4 @@ public class Math {
         return a * b;
     }
     
+}
