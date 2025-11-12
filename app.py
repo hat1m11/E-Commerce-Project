66135038da08ac8e7e1527f4e56e730f717a433c
Rohan Bhupla <rohanbhupla@gmail.com>
2025-11-12 12:39:20 +0000

    Routing

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/basket")
+def basket():
+    return render_template("basket.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
