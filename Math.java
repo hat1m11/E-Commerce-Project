6ff691a72aa3c840f251685a39afaa1c7dd3bed7
Hatim <hatimqazi2905@gmail.com>
2025-11-12 15:21:50 +0000

    Created math class, made sum method and made a templete

---
+++ Math.java
@@ -0,0 +1,57 @@
+public class Math {
+
+    /********************************
+     Developer: Hatim Qazi Haider Hussain
+     University ID: 240204258
+     Function: This function sums two numbers and returns the result.
+    ********************************/
+    
+    public int sum(int a, int b){
+        return a + b; 
+    }
+
+    /********************************
+     Developer: Yusuf
+     University ID: [Team Member 1 ID]
+     Function: Describe what your function does here (e.g., returns the maximum of two numbers)
+    ********************************/
+    // Write your function implementation here
+
+
+
+    /********************************
+     Developer: Zain
+     University ID: [Team Member 2 ID]
+     Function: Describe what your function does here
+    ********************************/
+    // Write your function implementation here
+
+
+
+    /********************************
+     Developer: Wafa
+     University ID: [Team Member 3 ID]
+     Function: Describe what your function does here
+    ********************************/
+    // Write your function implementation here
+
+
+
+    /********************************
+     Developer: Rohan
+     University ID: [Team Member 4 ID]
+     Function: Describe what your function does here
+    ********************************/
+    // Write your function implementation here
+
+
+
+    /********************************
+     Developer: Arifa
+     University ID: [Team Member 5 ID]
+     Function: Describe what your function does here
+    ********************************/
+    // Write your function implementation here
+
+}
+    
