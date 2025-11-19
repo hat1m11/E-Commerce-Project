f01b6253c87ed4fff1bb38de4c1b82b885863e8f
Wafa676 <230018613@aston.ac.uk>
2025-11-19 17:55:41 +0000

    editing pages

--- app.py
+++ app.py
@@ -5,5 +5,23 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+    @app.route('/admin-login')
+    def admin_login():
+        return render_template("admin/admin_login.html")
+
+        @app.route('/customer/login')
+        def customer_login():
+            return render_template("customer/customer_login.html")
+
+        @app.route('/customer/signup')
+        def customer_signup():
+            return render_template("customer/customer_signup.html")
+
+            @app.route('admin/signup')
+            def admin_signup():
+                return render_template("admin/admin_signup.html")
+
+        
+
 if __name__ == "__main__":
     app.run(debug=True)
